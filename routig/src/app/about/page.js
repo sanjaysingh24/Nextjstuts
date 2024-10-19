79937edb1ec4e7a9dc7f3d18066abac9af68d111
sanju <Impactmindztechsolutions@gmail.com>
2024-10-19 13:23:57 +0530

    update

--- routig/src/app/about/page.js
+++ routig/src/app/about/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Link from 'next/link'
 const page = () => {
   return (
     <div>
-      <h1>Hi this is the about page my first routing</h1>
+      <h1 className="text-xl mt-5 px-5">Hi this is the about page my first routing</h1>
+      <Link href ="/login">Go to login page</Link>
     </div>
   )
 }
--- routig/src/app/login/page.js
+++ routig/src/app/login/page.js
@@ -1,9 +1,20 @@
+'use client'
 import React from 'react'
 
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 const page = () => {
+    const router = useRouter();
   return (
     <div>
       <h1>hi this is the login page</h1>
+      <Link href = "/about">go to about page</Link>
+
+      {/* now we have to use a button to navigate the page */}
+ <div>
+ <button onClick={()=>router.push('/about')} className="border-1 border-4">hello</button>
+ </div>
     </div>
   )
 }
