b82298c81cd0b30ed1be04508eaa43a91cc53aef
Impactmindz <impactmindztechsolutions@gmail.com>
2024-11-12 15:33:28 +0530

    dynamicmetadata

---
+++ dynamicmetadata/src/app/user/page.js
@@ -0,0 +1,10 @@
+export default function page(){
+    return(
+        <div>hello this one is the page</div>
+    )
+}
+export  function generateMetadata({params}){
+    return{
+        title:"User page title"
+    }
+}
